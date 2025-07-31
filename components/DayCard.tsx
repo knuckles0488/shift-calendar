e22451c7754d4f68e7ab8259021527573de24ca6
knuckles0488 <104701599+knuckles0488@users.noreply.github.com>
2025-07-30 23:43:10 -0700

    Delete components directory

--- components/DayCard.tsx
+++
@@ -1,4 +0,0 @@
-
-// This component is no longer used.
-// The new calendar view is rendered by components/Calendar.tsx and components/CalendarDay.tsx.
-// Day details are shown in components/DayDetailModal.tsx.
--- components/PdfGeneratorButton.tsx
+++
@@ -1,3 +0,0 @@
-
-// This component is no longer used.
-// Its functionality has been integrated into the new components/SettingsMenu.tsx component.
