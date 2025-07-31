f825d0ad87338a2f81e6f98ed5e6c23fc78a3774
knuckles0488 <104701599+knuckles0488@users.noreply.github.com>
2025-07-31 00:50:18 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'shift-planner-cache-v12';
+const CACHE_NAME = 'shift-planner-cache-v13';
 const urlsToCache = [
   './',
   './index.html',
@@ -8,7 +8,7 @@ const urlsToCache = [
   './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf-autotable.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf-autotable.umd.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
   'https://esm.sh/react@19.1.1',
   'https://esm.sh/react-dom@19.1.1/client'
